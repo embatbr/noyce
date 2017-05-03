5cbad0f73516795eb316f9e5ab606cfa1d631f8d
Eduardo Tenório <embatbr@gmail.com>
2017-05-03 01:19:30 -0300

    Adding Pin class.

--- components/basic.py
+++ components/basic.py
@@ -5,13 +5,44 @@
 import math
 
 
+class UnsetPinError(Exception):
+
+    def __init__(self, msg_prefix):
+        self.message = '{} must be set.'.format(msg_prefix)
+
+
+class Pin(object):
+
+    def __init__(self):
+        self.__voltage = None
+
+    def __str__(self):
+        ret = '{} Volts'.format(self.__voltage)
+
+    def set(self, voltage):
+        assert isinstance(voltage, int) or isinstance(voltage, float)
+
+        self.__voltage = voltage
+
+    def unset(self):
+        self.__voltage = None
+
+    def read(self):
+        return self.__voltage
+
+
 class Diode(object):
 
     def __init__(self, saturation_current, ideality_factor=1):
+        assert 1 <= ideality_factor <= 2, 'Idealty factor must be between 1 and 2.'
+
         self.saturation_current = saturation_current
         self.max_fwd_ddp = 0.7
         self.ideality_factor = ideality_factor
         self.thermal_ddp = 0.02585
+
+        self.anode = Pin()
+        self.cathode = Pin()
 
     def __str__(self):
         ret = 'Is = {}\nmax V = {}\nn = {}\nVt = {}'.format(
@@ -23,7 +54,15 @@ class Diode(object):
 
         return ret
 
-    def calculate_current(self, fwd_ddp):
+    def calculate_current(self):
+        anode_voltage = self.anode.read()
+        cathode_voltage = self.cathode.read()
+
+        if (anode_voltage is None) or (cathode_voltage is None):
+            raise UnsetPinError('Anode and cathode')
+
+        fwd_ddp = anode_voltage - cathode_voltage
+
         if fwd_ddp > self.max_fwd_ddp:
             fwd_ddp = self.max_fwd_ddp
 
@@ -35,25 +74,55 @@ class Diode(object):
 if __name__ == '__main__':
     diode = Diode(25 / 1000000000)
     print(diode)
-
-    print('\nfwd_ddp: 0.4')
-    print('current:', diode.calculate_current(0.4))
-
-    print('\nfwd_ddp: 1')
-    print('current:', diode.calculate_current(1))
-
-    print('\nfwd_ddp: -0.1')
-    print('current:', diode.calculate_current(-0.1))
-
     print()
+
+    try:
+        diode.calculate_current()
+    except UnsetPinError as err:
+        print(err.message)
+
+    diode.anode.set(0.2)
+    diode.cathode.set(-0.2)
+    print('\nanode: {}'.format(diode.anode.read()))
+    print('cathode: {}'.format(diode.cathode.read()))
+    print('current:', diode.calculate_current())
+
+    diode.anode.set(1)
+    diode.cathode.set(0)
+    print('\nanode: {}'.format(diode.anode.read()))
+    print('cathode: {}'.format(diode.cathode.read()))
+    print('current:', diode.calculate_current())
+
+    diode.anode.set(1)
+    diode.cathode.set(1.1)
+    print('\nanode: {}'.format(diode.anode.read()))
+    print('cathode: {}'.format(diode.cathode.read()))
+    print('current:', diode.calculate_current())
+
+    print('\n===============\n')
     diode = Diode(25 / 1000000000, 1.3)
     print(diode)
+    print()
 
-    print('\nfwd_ddp: 0.4')
-    print('current:', diode.calculate_current(0.4))
+    try:
+        diode.calculate_current()
+    except UnsetPinError as err:
+        print(err.message)
 
-    print('\nfwd_ddp: 1')
-    print('current:', diode.calculate_current(1))
+    diode.anode.set(0.2)
+    diode.cathode.set(-0.2)
+    print('\nanode: {}'.format(diode.anode.read()))
+    print('cathode: {}'.format(diode.cathode.read()))
+    print('current:', diode.calculate_current())
 
-    print('\nfwd_ddp: -0.1')
-    print('current:', diode.calculate_current(-0.1))
+    diode.anode.set(1)
+    diode.cathode.set(0)
+    print('\nanode: {}'.format(diode.anode.read()))
+    print('cathode: {}'.format(diode.cathode.read()))
+    print('current:', diode.calculate_current())
+
+    diode.anode.set(1)
+    diode.cathode.set(1.1)
+    print('\nanode: {}'.format(diode.anode.read()))
+    print('cathode: {}'.format(diode.cathode.read()))
+    print('current:', diode.calculate_current())
